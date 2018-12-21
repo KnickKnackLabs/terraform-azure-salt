e2524efc27608dda474a5ee5ade85901c6f40b9c
Or Ricon <rikonor@gmail.com>
2018-12-21 14:11:10 -0500

    Use South Central repo

--- scripts/setup_walmart_apt_repos.sh
+++ scripts/setup_walmart_apt_repos.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 # Set Walmart Azure apt repos
 cat > /etc/apt/sources.list <<- EOF
-	deb http://aze2repo.cloud.wal-mart.com/ubuntu/ xenial main restricted universe multiverse
-	deb http://aze2repo.cloud.wal-mart.com/ubuntu/ xenial-updates main restricted universe multiverse
-	deb http://aze2repo.cloud.wal-mart.com/ubuntu/ xenial-security main restricted universe multiverse
+	deb http://azscrepo.cloud.wal-mart.com/ubuntu/ xenial main restricted universe multiverse
+	deb http://azscrepo.cloud.wal-mart.com/ubuntu/ xenial-updates main restricted universe multiverse
+	deb http://azscrepo.cloud.wal-mart.com/ubuntu/ xenial-security main restricted universe multiverse
 EOF
