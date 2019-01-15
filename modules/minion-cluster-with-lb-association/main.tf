44ff55dbaf8219b48570f8f0f719ed9f7e337754
Or Ricon <rikonor@gmail.com>
2019-01-15 12:28:01 -0500

    copy over minion-cluster module

---
+++ modules/minion-cluster-with-lb-association/main.tf
@@ -0,0 +1,130 @@
+provider "azurerm" {
+  subscription_id = "${var.subscription_id}"
+}
+
+resource "azurerm_network_interface" "main" {
+  count = "${var.num_of_minions}"
+
+  name                = "${var.prefix}-${var.name}-${count.index + 1}-nic"
+  resource_group_name = "${var.resource_group_name}"
+  location            = "${var.location}"
+  tags                = "${var.tags}"
+
+  ip_configuration {
+    name                          = "${var.prefix}-${var.name}-${count.index + 1}-ip-configuration"
+    subnet_id                     = "${var.subnet_id}"
+    private_ip_address_allocation = "dynamic"
+  }
+}
+
+data "template_file" "minion_config" {
+  count = "${var.num_of_minions}"
+
+  template = "${file("${path.module}/files/minion.tpl")}"
+
+  vars {
+    minion_id      = "${var.prefix}-${var.name}-${count.index + 1}"
+    master_address = "${var.salt_master_address}"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  count = "${var.num_of_minions}"
+
+  name                  = "${var.prefix}-${var.name}-${count.index + 1}-vm"
+  resource_group_name   = "${var.resource_group_name}"
+  location              = "${var.location}"
+  vm_size               = "${var.instance_size}"
+  network_interface_ids = ["${azurerm_network_interface.main.*.id[count.index]}"]
+
+  delete_os_disk_on_termination    = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "${var.prefix}-${var.name}-${count.index + 1}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}-${var.name}-${count.index + 1}"
+    admin_username = "${var.auth["user"]}"
+    admin_password = "disabled"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/${var.auth["user"]}/.ssh/authorized_keys"
+      key_data = "${var.auth["public_key"]}"
+    }
+  }
+
+  connection {
+    user        = "${var.auth["user"]}"
+    private_key = "${var.auth["private_key"]}"
+    timeout     = "1m"
+  }
+
+  provisioner "remote-exec" {
+    script = "${path.module}/../../scripts/wait_cloud_init.sh"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/../../scripts/setup_walmart_apt_repos.sh"
+    destination = "/tmp/setup_walmart_apt_repos.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/setup_walmart_apt_repos.sh",
+      "sudo /tmp/setup_walmart_apt_repos.sh",
+    ]
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/../../scripts/install_salt.sh"
+    destination = "/tmp/install_salt.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/install_salt.sh",
+      "SALT_VERSION=${var.salt_version} sudo -E /tmp/install_salt.sh minion",
+    ]
+  }
+
+  provisioner "file" {
+    content     = "${jsonencode(var.grains)}"
+    destination = "/tmp/grains"
+  }
+
+  provisioner "file" {
+    content     = "${data.template_file.minion_config.*.rendered[count.index]}"
+    destination = "/tmp/minion"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv /tmp/grains /etc/salt/grains",
+      "sudo mv /tmp/minion /etc/salt/minion",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo service salt-minion restart",
+    ]
+  }
+
+  tags = "${var.tags}"
+}
---
+++ modules/minion-cluster-with-lb-association/variables.tf
@@ -0,0 +1,72 @@
+# REQUIRED PARAMETERS
+
+variable "subscription_id" {
+  description = "The Azure subscription ID"
+}
+
+variable "resource_group_name" {
+  description = "The name of the Azure resource group we will be deployed into. This RG should already exist"
+}
+
+variable "subnet_id" {
+  description = "The ID of the Subnet under which the instance will be available"
+}
+
+variable "salt_master_address" {
+  description = "The address the minion can use to reach the salt master"
+}
+
+variable "auth" {
+  description = "Credentials for accesing the machine"
+  type        = "map"
+
+  default = {
+    user        = ""
+    public_key  = ""
+    private_key = ""
+  }
+}
+
+# OPTIONAL PARAMETERS
+
+variable "location" {
+  description = "The Azure region the consul cluster will be deployed in"
+  default     = "South Central US"
+}
+
+variable "instance_size" {
+  description = "The instance size"
+  default     = "Standard_F2"
+}
+
+variable "prefix" {
+  description = "A prefix for the instance and all of its associated resources"
+  default     = "salt-minion"
+}
+
+variable "salt_version" {
+  description = "The version of Salt to insatll"
+  default     = "latest"
+}
+
+variable "name" {
+  description = "A name for the minion"
+  default     = "generic"
+}
+
+variable "num_of_minions" {
+  description = "The number of minions to spawn"
+  default     = 1
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Any tags which should be assigned to the resources"
+}
+
+variable "grains" {
+  type        = "map"
+  default     = {}
+  description = "Grains to set on the minion"
+}
