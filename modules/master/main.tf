c9f77ca2c32297f3293d2ca13ae11812526132eb
Or Ricon <or.ricon@walmartlabs.com>
2020-11-17 10:20:38 -0500

    update modules to tf0.12

--- modules/master/main.tf
+++ modules/master/main.tf
@@ -1,89 +1,85 @@
-provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.35.0"
+    }
+  }
+}
+
+locals {
+  scripts_path = "${path.module}/../../scripts"
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-${var.name}-nic"
-  resource_group_name = "${var.resource_group_name}"
-  location            = "${var.location}"
-  tags                = "${var.tags}"
-
-  network_security_group_id = "${var.network_security_group_id}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags                = var.tags
 
   ip_configuration {
     name                          = "${var.prefix}-${var.name}-ip-configuration"
-    subnet_id                     = "${var.subnet_id}"
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "dynamic"
-
-    application_security_group_ids = ["${var.application_security_group_ids}"]
   }
 
   lifecycle {
-    ignore_changes = ["tags"]
+    ignore_changes = [tags]
   }
 }
 
-data "template_file" "minion_config" {
-  template = "${file("${path.module}/files/minion.tpl")}"
-
-  vars {
-    minion_id      = "${var.prefix}-${var.name}"
-    master_address = "localhost"
-  }
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = var.network_security_group_id
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-${var.name}-vm"
-  resource_group_name   = "${var.resource_group_name}"
-  location              = "${var.location}"
-  vm_size               = "${var.instance_size}"
-  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+resource "azurerm_network_interface_application_security_group_association" "main" {
+  network_interface_id          = azurerm_network_interface.main.id
+  application_security_group_id = var.application_security_group_id
+}
 
-  delete_os_disk_on_termination    = true
-  delete_data_disks_on_termination = true
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "${var.prefix}-${var.name}-vm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.instance_size
+  tags                = var.tags
 
-  storage_image_reference {
+  network_interface_ids = [azurerm_network_interface.main.id]
+
+  admin_username = var.auth["user"]
+  admin_ssh_key {
+    username   = var.auth["user"]
+    public_key = var.auth["public_key"]
+  }
+
+  source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
-  storage_os_disk {
-    name              = "${var.prefix}-${var.name}-osdisk"
-    disk_size_gb      = "${var.disk_size_gb}"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "${var.managed_disk_type}"
-  }
-
-  os_profile {
-    computer_name  = "${var.prefix}-${var.name}"
-    admin_username = "${var.auth["user"]}"
-    admin_password = "disabled"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-
-    ssh_keys {
-      path     = "/home/${var.auth["user"]}/.ssh/authorized_keys"
-      key_data = "${var.auth["public_key"]}"
-    }
+  os_disk {
+    name                 = "${var.prefix}-${var.name}-osdisk"
+    disk_size_gb         = var.disk_size_gb
+    storage_account_type = var.storage_account_type
+    caching              = "ReadWrite"
   }
 
   connection {
-    user        = "${var.auth["user"]}"
-    private_key = "${var.auth["private_key"]}"
+    host        = azurerm_network_interface.main.private_ip_address
+    user        = var.auth["user"]
+    private_key = var.auth["private_key"]
     timeout     = "1m"
   }
 
   provisioner "remote-exec" {
-    script = "${path.module}/../../scripts/wait_cloud_init.sh"
+    script = "${local.scripts_path}/wait_cloud_init.sh"
   }
 
   provisioner "file" {
-    source      = "${path.module}/../../scripts/setup_walmart_apt_repos.sh"
+    source      = "${local.scripts_path}/setup_walmart_apt_repos.sh"
     destination = "/tmp/setup_walmart_apt_repos.sh"
   }
 
@@ -95,7 +91,7 @@ resource "azurerm_virtual_machine" "main" {
   }
 
   provisioner "file" {
-    source      = "${path.module}/../../scripts/install_salt.sh"
+    source      = "${local.scripts_path}/install_salt.sh"
     destination = "/tmp/install_salt.sh"
   }
 
@@ -107,12 +103,15 @@ resource "azurerm_virtual_machine" "main" {
   }
 
   provisioner "file" {
-    content     = "${jsonencode(var.grains)}"
+    content     = jsonencode(var.grains)
     destination = "/tmp/grains"
   }
 
   provisioner "file" {
-    content     = "${data.template_file.minion_config.rendered}"
+    content = templatefile("${path.module}/files/minion.tmpl", {
+      minion_id      = "${var.prefix}-${var.name}"
+      master_address = "localhost"
+    })
     destination = "/tmp/minion"
   }
 
@@ -129,9 +128,7 @@ resource "azurerm_virtual_machine" "main" {
     ]
   }
 
-  tags = "${var.tags}"
-
   lifecycle {
-    ignore_changes = ["tags"]
+    ignore_changes = [tags]
   }
 }
--- modules/master/variables.tf
+++ modules/master/variables.tf
@@ -1,83 +1,81 @@
-# REQUIRED PARAMETERS
-
-variable "subscription_id" {
-  description = "The Azure subscription ID"
-}
-
 variable "resource_group_name" {
   description = "The name of the Azure resource group we will be deployed into. This RG should already exist"
+  type        = string
 }
 
 variable "subnet_id" {
   description = "The ID of the Subnet under which the instance will be available"
+  type        = string
 }
 
 variable "auth" {
   description = "Credentials for accesing the machine"
-  type        = "map"
-
-  default = {
-    user        = ""
-    public_key  = ""
-    private_key = ""
-  }
+  type = object({
+    user        = string
+    public_key  = string
+    private_key = string
+  })
 }
 
-# OPTIONAL PARAMETERS
-
 variable "location" {
-  description = "The Azure region the consul cluster will be deployed in"
-  default     = "South Central US"
+  description = "The Azure region master will be deployed in"
+  type        = string
 }
 
 variable "instance_size" {
   description = "The instance size"
+  type        = string
   default     = "Standard_F2"
 }
 
 variable "disk_size_gb" {
   description = "Specifies the size of the OS disk in GB."
+  type        = number
   default     = 30
 }
 
-variable "managed_disk_type" {
-  description = "Specifies the type of the managed OS disk"
+variable "storage_account_type" {
+  description = "Specifies the type of the OS disk"
+  type        = string
   default     = "Standard_LRS"
 }
 
 variable "network_security_group_id" {
   description = "The security group to put the network interface under"
-  default     = ""
+  type        = string
 }
 
-variable "application_security_group_ids" {
-  description = "The application security groups to put the network interface under"
-  default     = []
+variable "application_security_group_id" {
+  description = "The application security group to put the network interface under"
+  type        = string
 }
 
 variable "prefix" {
   description = "A prefix for the instance and all of its associated resources"
+  type        = string
   default     = "salt-master"
 }
 
 variable "salt_version" {
   description = "The version of Salt to insatll"
+  type        = string
   default     = "latest"
 }
 
 variable "name" {
   description = "A name for the master"
+  type        = string
   default     = "generic"
 }
 
 variable "tags" {
-  type        = "map"
-  default     = {}
   description = "Any tags which should be assigned to the resources"
+  type        = map(string)
+  default     = {}
 }
 
 variable "grains" {
-  type        = "map"
-  default     = {}
   description = "Grains to set on the minion"
+  type        = any
+  default     = {}
 }
