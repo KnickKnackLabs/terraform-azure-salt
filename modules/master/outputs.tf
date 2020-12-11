2cefa329a5df01efb0ffd111ed2a3c61a4272198
Or Ricon <or.ricon@walmartlabs.com>
2020-12-10 21:28:25 -0500

    expose both private and public master addresses

--- modules/master/outputs.tf
+++ modules/master/outputs.tf
@@ -1,3 +1,7 @@
-output "ip_address" {
+output "private_ip_address" {
+  value = azurerm_network_interface.main.private_ip_address
+}
+
+output "public_ip_address" {
   value = azurerm_public_ip.main.ip_address
 }
